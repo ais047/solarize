cc9c8db169ae4721d25cff1682b40bd2a67769fb
Dshollings <school@Davids-MacBook-Pro.local>
2017-09-14 12:49:12 -0700

    updated db fields and seed

--- db/userDB.sql
+++ db/userDB.sql
@@ -8,28 +8,26 @@ CREATE TABLE Users
 	id int NOT NULL AUTO_INCREMENT,
     user_name varchar(255) NOT NULL,
     user_password varchar(255) Not Null,
-    salt varchar(255) not null,
-	first_name varchar(255) NOT NULL,
-    last_name varchar(255) NOT NULL,
+    salt varchar(255) NOT NULL,
+	company_name varchar(255) NOT NULL,
     email varchar(255) NOT NULL,
     phone INT(10) NOT NULL,
-    address varchar(255) NOT NULL,
-    city varchar(255) NOT NULL,
-    state varchar(255)  NOT NULL,
-    zip INT(10) NOT NULL,
+    state varchar(2) NOT NULL,
 	PRIMARY KEY (id)
 );
 Drop table if exists Leads;
 CREATE TABLE Leads
 (
 	id int NOT NULL AUTO_INCREMENT,
-	company_name varchar(255) NOT NULL,
-    job_title varchar(255) NOT NULL,
+	first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
     phone INT(10) NOT NULL,
-    contacted boolean NOT NULL,
-    address varchar(255) NOT NULL,
+    contacted boolean default NULL,
+    converted boolean default NULL,
+    address varchar(255) default NULL,
     city varchar(255) NOT NULL,
-    state varchar(255)  NOT NULL,
-    zip INT(10) NOT NULL,
+    state varchar(2)  NOT NULL,
+    zip varchar(12)  NOT NULL,
 	PRIMARY KEY (id)
 );
